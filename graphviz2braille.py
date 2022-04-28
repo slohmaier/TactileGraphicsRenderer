49db8d39eeb40fe5c298161cb72a4e4ac66e4457
Stefan Lohmaier <stefan@slohmaier.de>
2022-04-28 21:03:31 +0200

    make qgridlayout a centralwidget

--- graphviz2braille.py
+++ graphviz2braille.py
@@ -1,13 +1,14 @@
 import sys
-from PySide6.QtWidgets import QMainWindow, QGridLayout, QLabel, QApplication
+from PySide6.QtWidgets import QMainWindow, QGridLayout, QLabel, QApplication, QWidget
 
 class TactileSchematics(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
-        self._grid = QGridLayout()
-        self.setLayout(self._grid)
-        self._grid.addWidget(QLabel(self.tr('HELLO')))
+        widget = QWidget()
+        self._grid = QGridLayout(widget)
+        self.setCentralWidget(widget)
+        self._grid.addWidget(QLabel('HELLO'), 0, 0)
 
 if __name__ == '__main__':
     app = QApplication([])
