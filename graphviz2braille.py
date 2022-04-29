e271eea203589aab9492d0b0e837977d9eb80a10
Stefan Lohmaier <stefan@slohmaier.de>
2022-04-29 10:04:39 +0200

    add translation tables and liblouis

--- graphviz2braille.py
+++ graphviz2braille.py
@@ -1,14 +1,16 @@
+import os
 import sys
+import louis
 
 from PySide6.QtWidgets import QMainWindow, QGridLayout, QLabel, QApplication, QWidget
 from PySide6.QtWidgets import QLineEdit, QPushButton, QHBoxLayout, QComboBox, QSpacerItem
-from PySide6.QtWidgets import QFontComboBox
+from PySide6.QtWidgets import QFontComboBox, QSizePolicy
 
 def create_hspacer() -> QSpacerItem:
     return QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
 class TactileSchematics(QMainWindow):
-    def __init__(self) -> None:
+    def __init__(self, louisDataPath) -> None:
         super().__init__()
 
         widget = QWidget()
@@ -37,6 +39,17 @@ class TactileSchematics(QMainWindow):
                 self._fonts.addItem(fc.itemText(i))
             i += 1
         hbox.addWidget(self._fonts)
+        hbox.addItem(create_hspacer())
+        self._grid.addLayout(hbox, row, 1)
+        row += 1
+
+        self._grid.addWidget(QLabel(self.tr('Translation Table:')), row, 0)
+        hbox = QHBoxLayout()
+        self._tables = QComboBox()
+        for tableFile in os.listdir(os.path.join(louisDataPath, 'liblouis', 'tables')):
+            self._tables.addItem(tableFile)
+        hbox.addWidget(self._tables)
+        hbox.addItem(create_hspacer())
         self._grid.addLayout(hbox, row, 1)
         row += 1
     
@@ -44,7 +57,11 @@ class TactileSchematics(QMainWindow):
         pass
 
 if __name__ == '__main__':
+    dataPath = os.path.dirname(__file__).encode('utf-8')
+    if dataPath != louis.liblouis.lou_setDataPath(dataPath):
+        raise Exception('Cannot configure liblouis!')
+
     app = QApplication([])
-    window = TactileSchematics()
+    window = TactileSchematics(dataPath.decode('utf-8'))
     window.show()
     sys.exit(app.exec())
