d258da14890015c223cf945ecdc326ca9cc1a5b6
Stefan Lohmaier <stefan@slohmaier.de>
2022-04-29 13:56:43 +0200

    improve shell script for general use

--- graphviz2braille.sh
+++ graphviz2braille.sh
@@ -1,4 +1,14 @@
-dot -Tsvg "-Nfontname=Braille29 DE ASCII" -Nfontsize=29 \
-    "-Efontname=Braille29 DE ASCII" -Efontsize=29 \
-    test1.dot > test1.svg
-open test1.svg
+#!/bin/sh
+if [ "$1" = "-h" ]; then
+    echo "$0 [INPUT FOT] [OUTPUT SVG]"
+    exit 0
+fi
+
+if [ ! -e "$1" ] || [ "x$2" = "x" ]; then
+    echo "Error: Try \$0 -h."
+    exit 1
+fi
+
+dot -Tsvg "-Nfontname=Braille29 DE" -Nfontsize=29 \
+    "-Efontname=Braille29 DE" -Efontsize=29 \
+    $1 > $2
